40ad9ad1239dc258796d57fbde982ec1e97cecab
Prathyush <prathyush.manoharan1@gmail.com>
2018-01-08 16:37:20 +0530

    min

--- src/app/geojson-leaflet/geojson-layer/geojson-layer.component.ts
+++ src/app/geojson-leaflet/geojson-layer/geojson-layer.component.ts
@@ -48,9 +48,9 @@ slices:any[];
 
 
 
-x:any;
-y:any;
-z:any;
+x:any=0;
+y:any=0;
+z:any=1;
 
 
 @Input()
@@ -62,20 +62,20 @@ indexMaxZoom:number=5;
 
 
 @Input()
-zoom:number=14;
+zoom:number=18;
 
 
 @Input()
-minZoom:number=10;
+minZoom:number=16;
 
 @Input()
-maxZoom:number=20;
+maxZoom:number=22;
 
 @Input()
 vectorTileStyling:any=layerStyles;
 
-@Input()
-vectortileUrl:string="https://free-{s}.tilehosting.com/data/v3/{z}/{x}/{y}.pbf.pict?key={key}";
+// @Input()
+// vectortileUrl:string="http://{s}.tile.osm.org/{z}/{x}/{y}.png";
 
 @Input()
 vectortileOptions:any={
@@ -87,13 +87,13 @@ vectortileOptions:any={
 };
 
 @Input()
-tilesUrl:string="http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
+tilesUrl:string="http://mapwarper.net/maps/tile/21937/{z}/{x}/{y}.png"
 
 
+@Input()
+center:number[]= [47.4330331,19.261177];
 
-center:number[]= [47.4384587,19.2501018];
-
-		
+drawnItems:any;		
 		
 
 
@@ -103,104 +103,19 @@ center:number[]= [47.4384587,19.2501018];
   constructor() {}
 
   ngOnInit(){
-  	//this.addStyles();
- 	this.map = L.map(this.el.nativeElement).setView(this.center, this.zoom);
-  this.map.options.minZoom = this.minZoom;
- 	this.map.options.maxZoom = this.maxZoom;
- 
+  	
+  	
+   this.map = L.map('map').setView(this.center, this.zoom);
 
-
-	
-
-	//L.vectorGrid.protobuf(this.tileUrl, this.tileOptions).addTo(this.map);
-
-	var mapLayer = L.tileLayer(this.tilesUrl, {
-		maxZoom: 18,
-		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-	});
-	mapLayer.addTo(this.map);
-	
-    var myLayer = L.geoJSON().addTo(this.map);
-	myLayer.addData(geojson);
-
-
-  let div = document.createElement('div');
-  let marker = L.marker( [47.4384587,19.2501018] )
-      .bindPopup('div.appendChild(markerRef.location.nativeElement)')
-      .addTo( this.map );
-
+   L.tileLayer(this.tilesUrl, {
+         minZoom: this.minZoom, maxZoom: this.maxZoom,
+         attribution: 'Insuide Tiles from mapwarper'
+     }).addTo(this.map);
   
 
+      
 
 
-
-
-var geoJsonDocument = {
-    type: 'FeatureCollection',
-    features: []
-};
-L.vectorGrid.slicer(geoJsonDocument, {
-    vectorTileLayerStyles: {
-        sliced: {}
-    }
-}).addTo(this.map);
-
-
-   // build an initial index of tiles
-   var tileIndex = geojsonvt(geojson);
-
-   // request a particular tile
-   var features = tileIndex.getTile(this.z, this.x, this.y).features;
-
-   // show an array of tile coordinates created so far
-   console.log(tileIndex.tileCoords); // [{z: 0, x: 0, y: 0}, ...]  
-
-
-  var tileIndex = geojsonvt(this.data, {
-  maxZoom: 18,  // max zoom to preserve detail on; can't be higher than 24
-  tolerance: 3, // simplification tolerance (higher means simpler)
-  extent: 4096, // tile extent (both width and height)
-  buffer: 64,    // tile buffer on each side
-  debug: 0,     // logging level (0 to disable, 1 or 2)
-  indexMaxZoom: 5,       // max zoom in the initial tile index
-  indexMaxPoints: 100000 // max number of points per tile in the index
-});
-
-
-
-
-//let markerRef:ComponentRef<MarkerComponentComponent>;
-//var map = L.map('this.map').setView([47.4384587,19.2501018 ], 18);
-
-
-//var myIcon = L.divIcon({className: 'marker-blk'});
-// you can set .my-div-icon styles in CSS
-//L.marker([47.4384587,19.2501018], {icon: myIcon}).addTo(this.map);
-
-
-
-//var cities = L.layerGroup([this.littleton, this.denver, this.aurora, this.golden]);
-
-//var grayscale = L.tileLayer(this.mapboxUrl, {id: 'MapID', attribution: this.mapboxAttribution}),
-    //streets   = L.tileLayer(this.mapboxUrl, {id: 'MapID', attribution: this.mapboxAttribution});
-
-//var map = L.map('this.map', {
-    //center: [47.4384587,19.2501018],
-   // zoom: 10,
-    //layers: [grayscale, cities]
-//});
-
-
-//var baseMaps = {
-    //"Grayscale": grayscale,
-    //"Streets": streets
-//};
-
-//var overlayMaps = {
-    //"Cities": cities
-//};
-
-//L.control.layers(baseMaps, overlayMaps).addTo(map);
 
 
 
